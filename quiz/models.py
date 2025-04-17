ec7f4078dc4d104a80794a5b8dcf8ad429c43571
arcaspo <caspo.alex@gmail.com>
2025-04-17 16:08:06 +1000

    Started on making the models required for the quiz database

---
+++ quiz/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Quiz(models.Model):
+    quiz_name = models.CharField(max_length=200)
+    due_date = models.DateTimeField("data due")
+
+    def __str__(self):
+        return self.quiz_name
+
+# Create your models here.
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    multiple_answers = models.BooleanField()
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.question
