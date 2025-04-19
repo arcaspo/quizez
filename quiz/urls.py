f18857f445a04e646b29294aceff701d4cd76677
arcaspo <caspo.alex@gmail.com>
2025-04-19 18:09:58 +1000

    Added logic and urls for new view  functions which render a simple dashboard with all quizzes and to display a question that hasnt been answered yet

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "urls"
+app_name = "quiz"
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
+    path("<int:quiz_id>/", views.question, name="question"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,33 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
+from .models import Quiz
+from .models import Question
+from .models import Answer
+from .models import Result
 
 # Create your views here.
 def dashboard(request):
-    return HttpResponse("yellow")
+    # get quizzes that are close to being due
+    due_quiz_list = Quiz.objects.order_by("-due_date")
+
+    # create the context that gets sent to the html file
+    context = {"due_quiz_list": due_quiz_list}
+
+    # render the html file
+    return render(request, "quiz/dashboard.html", context)
+
+def question(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    # results = get_list_or_404(Result, quiz=quiz)
+    questions = get_list_or_404(Question, quiz=quiz)
+
+    next_question: Question = None
+    # Find next question that hasnt been answered
+    for question in questions:
+        if not Result.objects.filter(quiz=quiz, question=question).exists():
+            if next_question is None:
+                next_question = question
+
+            elif next_question.order > question.order:
+                next_question = question
+
+    return render(request, "quiz/question.html", {"quiz": quiz, "question": next_question})
