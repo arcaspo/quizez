7c2a87e7d58582991bb6bef7e38814bdf38ff43b
arcaspo <caspo.alex@gmail.com>
2025-04-19 18:07:32 +1000

    Added result model and made some changes to other models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Quiz
+from .models import Question
+from .models import Answer
+from .models import Result
+
 
 # Register your models here.
 
 admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Result)
--- quiz/models.py
+++ quiz/models.py
@@ -1,26 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Quiz(models.Model):
     quiz_name = models.CharField(max_length=200)
-    due_date = models.DateTimeField("data due")
-    num_questions = models.IntegerField()
+    due_date = models.DateTimeField("due date")
 
     def __str__(self):
         return self.quiz_name
+    
+    # Property to get count dynamically
+    @property
+    def number_of_questions(self):
+        return self.question_set.count()
 
-# Create your models here.
 class Question(models.Model):
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, null=True)
-    question_text = models.CharField(max_length=200)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.TextField()
     multiple_answers = models.BooleanField()
+    order = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.question_text
+    
+    class Meta:
+        ordering = ['order']  # Order by the 'order' field
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     correct = models.BooleanField(default=False)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']  # Order by the 'order' field
 
     def __str__(self):
-        return self.question
+        return self.choice_text
+    
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Associate result with a user
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+    @property
+    def is_correct(self):
+        return self.answer.correct
