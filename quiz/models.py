98c5b81a4856d1e40601788e4f6f91123f10b516
arcaspo <caspo.alex@gmail.com>
2025-04-30 22:11:36 +1000

    tweaked quiz model to allow for editing

--- quiz/models.py
+++ quiz/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 class Quiz(models.Model):
     quiz_name = models.CharField(max_length=200)
-    due_date = models.DateTimeField("due date")
+    due_date = models.DateTimeField("due date", blank=True, null=True)
     quiz_description = models.TextField(blank=True, null=True)
+    editing = models.BooleanField(default=False)
 
     def __str__(self):
         return self.quiz_name
